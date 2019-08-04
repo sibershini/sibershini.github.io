7509e9d029eba2f92f06c9f70b16e30c59ad0690
sibershini <bryan.salas.j@gmail.com>
2019-08-03 23:31:18 -0400

    b

--- scripts/main.js
+++ scripts/main.js
@@ -2,10 +2,10 @@ var miImage = document.querySelector('img');
 
 miImage.onclick = function () {
     var miSrc = miImage.getAttribute('src');
-    if (miSrc === 'images/furry.jpg') {
-      miImage.setAttribute('src','images/furry2.jpg');
+    if (miSrc === 'images/meme.jpg') {
+      miImage.setAttribute('src','images/meme2.jpg');
     } else {
-      miImage.setAttribute('src', 'images/furry.jpg');
+      miImage.setAttribute('src', 'images/meme.jpg');
     }
 }
 var miBoton = document.querySelector('button');
@@ -13,14 +13,14 @@ var miTitulo = document.querySelector( 'h1');
 function estableceNombreUsuario() {
     var miNombre = prompt('Por favor, ingresa tu nombre.');
     localStorage.setItem('nombre', miNombre);
-    miTitulo.textContent = 'te esperaba, ' + miNombre+" oni-chan";
+    miTitulo.textContent = 'Yamete, ' + miNombre+" oni-chan";
 }
 if (!localStorage.getItem('nombre')) {
     estableceNombreUsuario();
 }
 else {
     var nombreAlmacenado = localStorage.getItem('nombre');
-    miTitulo.textContent = 'te esperaba ,' + nombreAlmacenado+" oni-chan";
+    miTitulo.textContent = 'Yamete ,' + nombreAlmacenado+" oni-chan";
 }
 miBoton.onclick = function() {
     estableceNombreUsuario();
